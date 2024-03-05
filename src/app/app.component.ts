078062f9fad5864f79f04aa92a4cce4d00c72a68
Vilius Kerpauskas <vilius.kerpauskas@gmail.com>
2024-03-05 21:24:09 +0200

    12. A Basic Project Setup using Bootstrap for Styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  name: string = 'Vilius';
-}
+export class AppComponent {}
